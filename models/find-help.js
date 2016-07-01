15dc66b35e3e633b0f52f02080adcf97da677866
erin cherney <erin.t.cherney@gmail.com>
2016-07-01 10:11:02 -0400

    ugh

---
+++ models/find-help.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+const dbConnection = 'mongodb://localhost:27017/tracker';
+const request = require('request');
+
+
+function getFacilityData(req,res,next) {
+  request({
+    url: 'https://data.cityofnewyork.us/resource/8nqg-ia7v.json?',
+    method: 'get',
+    qs: {
+      facility_type: 'MH',
+      zip: req.query.zip,
+    },
+    json:true
+  },
+  function (err, result, data) {
+    if (err) throw err;
+    res.facilityList = data;
+    next();
+  })
+}
+
+
+module.exports = { getFacilityData }
